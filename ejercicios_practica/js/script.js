0611489d0df518728e2c4bf5768a9ade8055e7e0
Valls <fernandodaniel.valls@externos-ar.cencosud.com>
2023-07-04 19:07:14 -0300

    en proceso

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -8,3 +8,26 @@
 /* Enunciado
     --> Leer el README para ver el enunciado
 */
+
+async function characterApi() {
+    try {
+        const response = await fetch('https://rickandmortyapi.com/api/character/?name=baby wizard');
+        const data = await response.json();
+        return data.results
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
+const dataApi = characterApi();
+
+dataApi.then(result => {
+    console.log("asd")
+    console.log(result);
+})
+    .catch(error => {
+        console.log('Ocurrió un error:', error);
+    });
+
+
+
