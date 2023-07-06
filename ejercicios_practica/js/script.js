60a1491cd82c4c48109696222be20f0bdaec4224
Valls <fernandodaniel.valls@externos-ar.cencosud.com>
2023-07-05 23:30:42 -0300

    terminado ejercicios de practica

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -1,22 +1,15 @@
 "use strict";
 
-/* Tarea
- * Objetivos: adquirir herramientas y poner
- * en práctica lo visto en clase
- */
-
-/* Enunciado
---> Leer el README para ver el enunciado
-*/
-
 let btnConsultar = document.getElementById('btnConsultar');
 let input = document.getElementById("personaje")
 let title = document.getElementById("title")
-let statusElement = document.getElementById('setStatus');
-
+let scriptElement = document.querySelector('.articleModels');
+let statusElement = scriptElement.querySelector('.status');
+let statusDotElement = scriptElement.querySelector('.status-dot');
 
 let inputName = ""
 let chapter = ""
+let statusText;
 
 let setStatus = document.getElementById("setStatus")
 let lastKnowLocation = document.getElementById("lastKnowLocation")
@@ -25,24 +18,12 @@ let image = document.getElementById("image")
 
 btnConsultar.onclick = async () => {
 
-    statusElement.textContent = ''
-
     async function characterApi() {
         try {
             const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${input.value}`);
             const data = await response.json();
 
             console.log(data.results);
-
-            // Dependiendo del estado setear el icono
-            statusElement.textContent = `${data.results[0].status} - ${data.results[0].species}`
-
-            if (statusElement.textContent.includes('Alive')) {
-                statusElement.classList.add('green');
-            } else {
-                statusElement.classList.add('red');
-            }
-            //Configuro las variables de la card con los datos de la api
             chapter = data.results[0].episode[0]
 
             setTimeout(() => {
@@ -51,15 +32,24 @@ btnConsultar.onclick = async () => {
                 image.src = data.results[0].image
             }, 200);
 
+            statusElement.innerHTML = '';
+            statusElement.appendChild(statusDotElement);
+            statusElement.appendChild(document.createTextNode(`${data.results[0].status} - ${data.results[0].species}`));
+
+            if (statusElement.textContent.includes('Alive')) {
+                statusDotElement.className = 'status-dot ' + "green"
+            } else {
+                statusDotElement.className = 'status-dot ' + "red"
+            }
+
         } catch (error) {
             alert("Error en la API")
-            console.log('Error:', "Error en la API");
         }
 
         try {
-            const response1 = await fetch(`${chapter}`);
-            const data1 = await response1.json();
-            firstSeenIn.textContent = data1.name
+            const resChapter = await fetch(`${chapter}`);
+            const dataChapter = await resChapter.json();
+            firstSeenIn.textContent = dataChapter.name
         } catch (error) {
             console.error('Error:', error);
             throw error;
